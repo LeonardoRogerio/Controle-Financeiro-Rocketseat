737069ea00c146383da251ca67cd2f1e62ca6c84
Leonardo Rogerio <leonardoanjoss1276@gmail.com>
2021-09-08 19:02:46 -0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createServer, Model } from 'miragejs'
+import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model, 
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [{
+        id: 1,
+        title: '',
+        type: 'deposit',
+        category: '',
+        amount: 0,
+        createdAt: new Date(''),
+      },
+      {
+        id: 2,
+        title: '',
+        type: 'withdrawn',
+        category: '',
+        amount: 0,
+        createdAt: new Date(''),
+        
+      }]
+    })
+  },
+
+
+  routes() {
+    this.namespace = 'api';
+
+
+    this.get('/transactions', () =>{
+      return this.schema.all('transaction')
+    })
+    
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  }
+})
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
+function deposit(deposit: any) {
+  throw new Error('Function not implemented.');
+}
+
